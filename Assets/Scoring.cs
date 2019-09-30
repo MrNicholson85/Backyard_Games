d7459afa7b906248b0b784767a9d02071aa76423
DarrylDidIt <darryl.a.nicholson@gmail.com>
2019-09-30 07:14:13 -0700

    Game Update
    > Level design
    > Basic UI
    > basic pointing system (Needs work)
    > OVR Player Controller

---
+++ Assets/Scoring.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+
+    public bool enter = false;
+
+    [SerializeField]
+    private UIManager _uiManager; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("Point");
+            _uiManager.UpdateRedScore();
+        }
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    private TMP_Text m_TextComponent;
+    public TextMeshProUGUI redScoreText;
+    public TextMeshProUGUI blueScoreText;
+
+    public int redScore;
+    public int blueScore;
+
+
+    private void Awake()
+    {
+        m_TextComponent = GetComponent<TMP_Text>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TextComponent.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateRedScore()
+    {
+        redScore += 1;
+        m_TextComponent.text = "Test" + redScore;
+    }
+}
