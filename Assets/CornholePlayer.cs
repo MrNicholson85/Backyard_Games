991ad502d296fe17c1d7a28b3ab4cf530b695b96
Darryl nicholson <darryl.nicholson@freshconsulting.com>
2019-10-01 21:05:21 -0700

    updated player started pause menu

---
+++ Assets/CornholePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornholePlayer : MonoBehaviour
+{
+
+    public GameObject playerUIMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerUIMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMenu();
+    }
+
+    public void PlayerMenu()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Three) || Input.GetKeyDown(KeyCode.Space))
+        {
+            playerUIMenu.SetActive(true);
+        }
+    }
+}
